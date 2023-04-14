70cbeec2bbb75218bedd2ab979511309a86bfc63
Your Name <you@example.com>
2023-04-15 04:07:09 +0530

    link blue change

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -87,15 +87,15 @@ export default function Sidebar() {
 {/* <!-- fixed nav --> */}
 <nav class="fixed h-20 bottom-0 inset-x-0 bg-white shadow-[5px_-5px_10px_-0px_rgba(0,0,0,0.1)] flex justify-between text-sm  capitalize font-mono">
 
-  <a href="#" class="font-semibold w-full block py-5 px-3 text-center hover:bg-blue-50 transition duration-300">
+  <div href="#" class="font-semibold w-full block py-5 px-3 text-center hover:bg-blue-50 transition duration-300">
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#0d1134" class="w-6 h-6 mb-2 mx-auto">
   <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clip-rule="evenodd" />
 </svg>
 
     Profile
-  </a>
+  </div>
 
-  <a href="#" class="font-semibold w-full block py-5 px-3 text-center hover:bg-blue-50">
+  <div href="#" class="font-semibold w-full block py-5 px-3 text-center hover:bg-blue-50">
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#0d1134" className="w-6 h-6 mb-2 mx-auto">
   <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
   <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
@@ -104,23 +104,23 @@ export default function Sidebar() {
 
 
     Dashboard
-  </a>
+  </div>
 
-  <a href="#" class="font-semibold w-full block py-5 px-3 text-center hover:bg-blue-50">
+  <div href="#" class="font-semibold w-full block py-5 px-3 text-center hover:bg-blue-50">
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#0d1134" class="w-6 h-6 mb-2 mx-auto">
   <path fill-rule="evenodd" d="M1.5 5.625c0-1.036.84-1.875 1.875-1.875h17.25c1.035 0 1.875.84 1.875 1.875v12.75c0 1.035-.84 1.875-1.875 1.875H3.375A1.875 1.875 0 011.5 18.375V5.625zm1.5 0v1.5c0 .207.168.375.375.375h1.5a.375.375 0 00.375-.375v-1.5a.375.375 0 00-.375-.375h-1.5A.375.375 0 003 5.625zm16.125-.375a.375.375 0 00-.375.375v1.5c0 .207.168.375.375.375h1.5A.375.375 0 0021 7.125v-1.5a.375.375 0 00-.375-.375h-1.5zM21 9.375A.375.375 0 0020.625 9h-1.5a.375.375 0 00-.375.375v1.5c0 .207.168.375.375.375h1.5a.375.375 0 00.375-.375v-1.5zm0 3.75a.375.375 0 00-.375-.375h-1.5a.375.375 0 00-.375.375v1.5c0 .207.168.375.375.375h1.5a.375.375 0 00.375-.375v-1.5zm0 3.75a.375.375 0 00-.375-.375h-1.5a.375.375 0 00-.375.375v1.5c0 .207.168.375.375.375h1.5a.375.375 0 00.375-.375v-1.5zM4.875 18.75a.375.375 0 00.375-.375v-1.5a.375.375 0 00-.375-.375h-1.5a.375.375 0 00-.375.375v1.5c0 .207.168.375.375.375h1.5zM3.375 15h1.5a.375.375 0 00.375-.375v-1.5a.375.375 0 00-.375-.375h-1.5a.375.375 0 00-.375.375v1.5c0 .207.168.375.375.375zm0-3.75h1.5a.375.375 0 00.375-.375v-1.5A.375.375 0 004.875 9h-1.5A.375.375 0 003 9.375v1.5c0 .207.168.375.375.375zm4.125 0a.75.75 0 000 1.5h9a.75.75 0 000-1.5h-9z" clip-rule="evenodd" />
 </svg>
 
 
     Watchlist
-  </a>
+  </div>
 
-  <a href="#" class="font-semibold w-full block py-5 px-3 text-center hover:bg-blue-50">
+  <div href="#" class="font-semibold w-full block py-5 px-3 text-center hover:bg-blue-50">
   <svg className="w-6 h-6 mb-2 mx-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
         </svg>
     Logout
-  </a>
+  </div>
 
 </nav>
   </div>
